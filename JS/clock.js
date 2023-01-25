01c2bc41554e523a2a81f8d512cf3d5fbf6fc0c8
yeong96 <69592002+doyeong1996@users.noreply.github.com>
2023-01-25 14:41:36 +0900

    js function onToday add

--- JS/clock.js
+++ JS/clock.js
@@ -1,5 +1,15 @@
 const clock = document.querySelector("#clock");
 const today = document.querySelector("#today");
+const day = document.querySelector("#day");
+
+function onToday() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // 0~11까지만 표시가된다 그래서 + 1 을 따로해준다.
+  const days = date.getDate();
+
+  day.innerText = `${year}-${month}-${days}`;
+}
 
 function onLiveClock() {
   const date = new Date();
@@ -9,7 +19,8 @@ function onLiveClock() {
 
   clock.innerText = `${hours} : ${minutes} : ${seconds}`;
 }
-//오늘 날짜로 자동 업데이트
-today.value = new Date().toISOString().substring(0, 10);
+//오늘 날짜로 자동 업데이트 input 값으로 했을시
+//today.value = new Date().toISOString().substring(0, 10);
+onToday();
 onLiveClock();
 setInterval(onLiveClock, 1000);
