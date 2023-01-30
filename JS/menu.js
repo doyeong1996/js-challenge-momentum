ffe0061e29379f1d8f78a31cc6c898a678afa684
yeong96 <69592002+doyeong1996@users.noreply.github.com>
2023-01-30 23:47:04 +0900

    menu.js modify
    주석처리 랜덤 id 값 생성 함수

--- JS/menu.js
+++ JS/menu.js
@@ -7,17 +7,18 @@ function paintList(listObj) {
 }
 
 function divClick() {
+  // div가 클릭되었을때 랜덤한 id를 부여하는 함수
   const divValue = menuListDiv.value;
   const listObj = {
     text: divValue,
     id: Date.now(),
   };
-  paintList(listObj);
+  paintList(listObj); //listobj를 인자로
 }
 
 function buttonClick() {
-  menuButton.classList.toggle("active-1");
-  menuList.classList.toggle("hidden");
+  menuButton.classList.toggle("active-1"); //toggle 클릭시 함수 on 한번 더 클릭시 전의값으로 돌아간다.
+  menuList.classList.toggle("hidden"); //toggle 클릭시 함수 on 한번 더 클릭시 전의값으로 돌아간다.
 }
 
 menuButton.addEventListener("click", buttonClick);
