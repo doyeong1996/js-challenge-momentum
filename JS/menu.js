3a22c5a0f6b16cbd9b9b701e3cdd23ea599fddeb
yeong96 <69592002+doyeong1996@users.noreply.github.com>
2023-02-01 00:11:02 +0900

    Function menu-game
    메뉴의 리스트중 게임을 클릭하면 화면중앙에 생기고 다시누르면 사라지는 기능  추가

--- JS/menu.js
+++ JS/menu.js
@@ -1,10 +1,14 @@
 const menuButton = document.querySelector(".menu-trigger");
 const menuList = document.querySelector("#content_menu_list");
-const menuListLi = menuList.querySelector("li");
-
+const randomGame = document.querySelector("#random-game");
+const game = document.querySelector(".content-game");
 function buttonClick() {
   menuButton.classList.toggle("active-1"); //toggle 클릭시 함수 on 한번 더 클릭시 전의값으로 돌아간다.
   menuList.classList.toggle("hidden"); //toggle 클릭시 함수 on 한번 더 클릭시 전의값으로 돌아간다.
 }
 
+function onRandomGame() {
+  game.classList.toggle("hidden");
+}
 menuButton.addEventListener("click", buttonClick);
+randomGame.addEventListener("click", onRandomGame);
