1189c93d7723b4a22ba2d06ded396001cf6fb82f
yeong96 <69592002+doyeong1996@users.noreply.github.com>
2023-02-03 11:57:44 +0900

    JS menu modify
    메뉴 X 표시 클릭 시 효과 추가

--- JS/menu.js
+++ JS/menu.js
@@ -5,6 +5,7 @@ const game = document.querySelector(".content-game");
 function buttonClick() {
   menuButton.classList.toggle("active-1"); //toggle 클릭시 함수 on 한번 더 클릭시 전의값으로 돌아간다.
   menuList.classList.toggle("hidden"); //toggle 클릭시 함수 on 한번 더 클릭시 전의값으로 돌아간다.
+  game.classList.add("hidden"); // x 표시 눌렀을경우 랜덤게임도 사라짐
 }
 
 function onRandomGame() {
